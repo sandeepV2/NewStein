c24c3abecf44ee1c751501bf7ca9316fa788a3ce
Sandeep Belagavi <sandeepb@qubole.com>
2019-08-10 13:04:53 +0530

    Adding display typescript

---
+++ Display.ts
@@ -0,0 +1,6 @@
+let my_first_git_time:Date = new Date();
+
+// Time i created my fist typescriptSat Aug 10 2019 13:03:20 GMT+0530 (India Standard Time)
+
+console.log("Time i created my fist typescript" + my_first_git_time);
+
